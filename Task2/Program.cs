f01979ba2c9baf3303539933519c5de82a888216
RomanKosov48 <kosovr48@gmail.com>
2023-04-02 14:57:12 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int max;
+
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+
+Console.WriteLine($"max = {max}");
